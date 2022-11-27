1dbe2db40f5507a0601363d01f8f04deb6103e5b
lftroq <109480517+lftroq@users.noreply.github.com>
2022-11-27 17:02:32 +0700

    Update example.py

--- cogs/example.py
+++ cogs/example.py
@@ -2,6 +2,13 @@ from utility import Cog, setup_cog
 import discord
 
 class Example(Cog):
+  # Your code here
+  # Begin
+  
+
+  # End
+
+  # EXAMPLE COMMAND
   @discord.app_commands.command(name="hello", description="Đẹp trai bố đời")
   async def introduce(self, interaction: discord.Interaction, name: str):
     # Defer to make sure command can finish before Interaction token expires
