d84167055877afe1a0f68142cdc09fa00af6e50c
lftroq <109480517+lftroq@users.noreply.github.com>
2022-11-27 17:14:58 +0700

    Add files via upload

--- utility.py
+++ utility.py
@@ -20,53 +20,6 @@ from discord import (
 )
 from discord.ext.commands import Cog as OriginalCog
 
-OLD_COLORS = {
-    "LFTROQ": "#ffc0cb",
-    "RED": "#ff0000",
-    "GREEN": "#00ff00",
-    "BLUE": "#0000ff",
-    "YELLOW": "#ffff00",
-    "PINK": "#ff00ff",
-    "PURPLE": "#8A2BE2",
-    "CYAN": "#00ffff",
-    "WHITE": "#ffffff",
-    "BLACK": "#000000",
-    "ORANGE": "#ffa500",
-    "BROWN": "#a52a2a",
-    "GREY": "#808080",
-    "DARK_GREY": "#404040",
-    "LIGHT_GREY": "#c0c0c0",
-    "DARK_GREEN": "#006400",
-    "LIGHT_GREEN": "#90ee90",
-    "DARK_BLUE": "#00008b",
-    "LIGHT_BLUE": "#add8e6",
-    "DARK_PURPLE": "#800080",
-    "LIGHT_PURPLE": "#e6e6fa",
-    "DARK_CYAN": "#008b8b",
-    "LIGHT_CYAN": "#00ffff",
-    "DARK_RED": "#8b0000",
-    "LIGHT_RED": "#ffb6c1",
-    "DARK_ORANGE": "#ff8c00",
-    "LIGHT_ORANGE": "#ffa07a",
-    "DARK_BROWN": "#a52a2a",
-    "LIGHT_BROWN": "#deb887",
-    "DARK_GOLD": "#ffd700",
-    "LIGHT_GOLD": "#ffefd5",
-    "DARK_SILVER": "#c0c0c0",
-    "LIGHT_SILVER": "#e0e0e0",
-    "DARK_PINK": "#ff1493",
-    "LIGHT_PINK": "#ffb6c1",
-    # Down here are legacy ones (Used for backwards compatibility / Usually not used because of their deprecations)
-    "C_RED": 0xff0000,
-    "C_GREEN": 0x00ff00,
-    "C_BLUE": 0x0000ff,
-    "C_YELLOW": 0xffff00,
-    "C_PURPLE": 0xff00ff,
-    "C_CYAN": 0x00ffff,
-    "C_WHITE": 0xffffff,
-    "C_BLACK": 0x000000
-}
-
 COLORS = {
     "TEAL": 0x1abc9c,
     "DARK_TEAL": 0x11806a,
@@ -102,6 +55,8 @@ COLORS = {
     "YELLOW": 0xfee75c
 }
 
+# Do "get_color(<COLOR'S ID ABOVE>) to get color"
+# If the color ID is invalid, the function will return #ff0000
 
 def get_color(color: str, type_to_convert_to: str = "hex") -> int:
   type_to_convert_to = type_to_convert_to.lower()
@@ -125,6 +80,8 @@ def get_color(color: str, type_to_convert_to: str = "hex") -> int:
     return int("ff0000", 16)
 
 
+# Get/save data with and without async
+
 def get_json_data(directory: str) -> Union[dict, set, list, tuple]:
   with open(directory, mode="r", encoding="utf-8") as file:
     return json.loads(file.read())
@@ -144,6 +101,7 @@ async def async_save_json_data(directory: str, data: Union[dict, set, list, tupl
   async with aiofiles.open(directory, mode="w", encoding="utf-8") as file:
     await file.write(json.dumps(data, indent=2, ensure_ascii=False))
 
+# Embed
 
 class Embed(OriginalEmbed):
   def __init__(self, title: str, color: Any, description: str, *args, **kwargs):
@@ -157,6 +115,8 @@ class Embed(OriginalEmbed):
     self.set_footer(
       text="Created by hanhdoo#2225 with 💖."
     )
+
+# Cog
 
 class Cog(OriginalCog):
   def __init__(self, client, *args, **kwargs) -> None:
@@ -172,6 +132,7 @@ def setup_cog(cog: Cog):
     await client.add_cog(cog(client), guild=Object(id=getenv("GUILD_ID")))
   return setup
 
+# Get file
 
 def get_files(directory: str, exceptions: Union[tuple, list, set] = []) -> list:
   return [
@@ -184,6 +145,7 @@ def get_sub_directories(directory: str, exceptions: Union[tuple, list, set] = []
       sub_directory for sub_directory in listdir(directory) if path.isdir(path.join(directory, sub_directory)) and sub_directory not in exceptions
   ]
 
+# Delete file
 
 def delete_file(directory: str) -> None:
   remove(directory)
